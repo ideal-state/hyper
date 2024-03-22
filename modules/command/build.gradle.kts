fe5ea9b2511f926bbd98390627431b123e620c04
ketikai <ketikai@idealstate.team>
2024-03-22 18:01:25 +0800

    build(modules,licenses): 初始化子模块与各模块许可证等，为接下来的项目迁移做好准备

---
+++ modules/command/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    hyper_java
+    hyper_maven_publish
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.0")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
